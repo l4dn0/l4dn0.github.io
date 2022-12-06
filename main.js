61b2b24e362ae64745f76df35963951c4d6a476e
MrArtemV <ves.artem2005@gmail.com>
2022-12-06 09:06:47 +0300

    delete without_(el_tatto) counter

--- main.js
+++ main.js
@@ -15,13 +15,9 @@ async function editImageUrl() {
 editImageUrl()
 
 let wodate = new Date(2022, 6, 30) // день закрытия квайтиленди
-let bodate = new Date(2022, 11, 3) // день бана El_tatto
 let now = new Date() // сегодня
 diff = Math.abs(wodate.getTime() - now.getTime()) / (1000 * 3600 * 24)
-bodiff = Math.abs(bodate.getTime() - now.getTime()) / (1000 * 3600 * 24)
 
 let without = document.querySelector('.without')
-let bogdan = document.querySelector('.bogdan')
 
 without.innerHTML = Math.floor(diff)
-bogdan.innerHTML = Math.floor(bodiff)
